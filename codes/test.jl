4e79e0d9e3cdcb0959aa6837800ee5bd457f16b7
Minah Yang <lucia.yang@colorado.edu>
2019-06-10 08:47:11 -0600

    change true RK4 solve to have h=1e-6

--- codes/test.jl
+++ codes/test.jl
@@ -2,28 +2,24 @@ include("RT.jl")
 using PyPlot
 seed=12
 Random.seed!(seed);
-#T = 1000; 
-#ϵ = 0.001;
 h = exp10(-3); 
-
 ω = intRT(3)
 C = floatRT(5)
 IC = onUnitCircle(3);
 #RK4_step, EUimex_step
+R = collect(2:4)
 
 for i in R
 	name="true"*string(i)
-	T = exp10(i); ϵ=exp10(-i); h=exp10(-8)
+	T = exp10(i); ϵ=exp10(-i); h=exp10(-6)
 	N = Int(ceil(T/h)); 
 	every = Int(ceil(N/100)) #only save 101 values total
 	RT_amp(N, h, every, IC, ω=ω, ϵ=ϵ, C=C, stepper=RK4_step, name=name);	
 end
 
-
 steppers = [IFE_step, ETD1_step, CNimex_step]
 colors = ["r","b","g"]
 ss = length(steppers)
-R = collect(2:3) 
 for i in R
 	for j = 1 : ss
 		name=string(steppers[j])*string(i)
