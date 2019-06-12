96c0301e8d1c6cddb224042e8bf58be662b71d7d
Minah <minah@github.com>
2019-06-11 20:50:40 -0600

    test 2:3 with true eval at h=1e-4

--- codes/test.jl
+++ codes/test.jl
@@ -1,21 +1,21 @@
 include("RT.jl")
 using PyPlot
-seed=12
+seed=13
 Random.seed!(seed);
 h = exp10(-3); 
 ω = intRT(3)
 C = floatRT(5)
 IC = onUnitCircle(3);
 #RK4_step, EUimex_step
-R =[4]
+R = collect(2:3)
+
 for i in R
 	name="true"*string(i)
-	T = exp10(i); ϵ=exp10(-i); h=exp10(-6)
+	T = exp10(i); ϵ=exp10(-i); h=exp10(-4)
 	N = Int(ceil(T/h)); 
 	every = Int(ceil(N/100)) #only save 101 values total
 	RT_amp(N, h, every, IC, ω=ω, ϵ=ϵ, C=C, stepper=RK4_step, name=name);	
 end
-R = collect(2:3)
 
 steppers = [IFE_step, ETD1_step, CNimex_step]
 colors = ["r","b","g"]
@@ -32,12 +32,14 @@ end
 
 for k = 1 : 3
 	for i in R
+		T = exp10(i); x = range(0, T, length=101)
 		tsol = readdlm("true"*string(i)*".txt")
 		print(100+length(R)*10+i-1, "\n")
 		subplot(100+length(R)*10+i-1)
 		for j = 1 : ss
 			name=string(steppers[j])*string(i)
-			semilogy(abs.(tsol[:,k]-readdlm(name*".txt")[:,k]),c=colors[j], label=name)
+			semilogy(x, abs.(tsol[:,k]-readdlm(name*".txt")[:,k]),
+				c=colors[j], label=name)
 		end
 		legend()
 	end
@@ -50,15 +52,27 @@ colors = ["r","b","g","k"]
 ss = length(steppers)
 for k = 1 : 3
 	for i in R
+		T = exp10(i); x = range(0, T, length=101)
 		tsol = readdlm("true"*string(i)*".txt")
 		print(100+length(R)*10+i-1, "\n")
 		subplot(100+length(R)*10+i-1)
 		for j = 1 : ss
 			name=string(steppers[j])*string(i)
-			plot(readdlm(name*".txt")[:,k],c=colors[j], label=name)
+			plot(x, readdlm(name*".txt")[:,k],c=colors[j], label=name)
 		end
 		legend()
 	end
 	savefig("seed"*string(seed)*"-z"*string(k)*".png")
 	close()
+end
+
+for i in R
+	T = exp10(i); x = range(0, T, length=101)
+	for j = 1 : ss
+		name=string(steppers[j])*string(i)
+		plot(x, readdlm(name*".txt"),c=colors[j], label=name)
+		legend()
+		savefig("everything"*string(i)*string(steppers[j])*".png")
+		close()
+	end
 end
