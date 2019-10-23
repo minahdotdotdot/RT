5cf38b2e4d0fffc4dfe0d2f8884eed7a68ec75c0
Minah Yang <lucia.yang@colorado.edu>
2019-10-23 10:53:24 -0600

    fix exponentiation to keep zero elements zero

--- codes/MMT.jl
+++ codes/MMT.jl
@@ -31,37 +31,42 @@ struct eRKTableau
     eRKTableau(A, b, c) = new(copy(A),copy(b), copy(c))
 end
 
+@inline function expnz(z::Array{Complex{T},1}) where T<: AbstractFloat
+     z[z.!=0] = exp.(z[z.!=0])
+     return z
+ end
+
 function IFRK_step(z::Array{ComplexF64,1}, h::Float64, 
-    L, NLfunc::Function, nlfP::funcparams, 
+    L, NLfunc::Function, fP::funcparams, 
     RKT::eRKTableau, ks, k, FD)
     #k = zeros(eltype(z), length(RKT.b), length(z))
-    ks[1,:] = NLfunc(z, nlfP, k, FD)
+    ks[1,:] = NLfunc(z, fP, k, FD)
     for i = 2 :length(RKT.b)
         PP=h*Transpose(ks[1:i-1,:])*RKT.A[i,1:i-1]
-        ks[i,:] = exp.(-h*RKT.c[i]*L) .* NLfunc(exp.(h*RKT.c[i]*L) .*(z + PP), nlfP, k, FD)
+        ks[i,:] = expnz(-h*RKT.c[i]*L) .* NLfunc(expnz(h*RKT.c[i]*L) .*(z + PP), fP, k, FD)
     end
     vb = Transpose(ks)*b
     #return k, vb, L * (z+ (h*vb))
-    return exp.(h*L) .* (z+ (h*vb))
+    return expnz(h*L) .* (z+ (h*vb))
 end
 
 function IFRK!(M::Int, every::Int, IC::Array{ComplexF64,1}, h::Float64, 
     L, NLfunc::Function, fP::funcparams, RKT::eRKTableau, k; name::String)
     # FFT into Fourier space
-    zhat = fft(IC)*0.0001
-    newtxt!(zhat, name=name)
-    ks = zeros(eltype(zhat), length(RKT.b), length(IC)) #RK stages (allocate memory)
+    zhat = fft(IC)*0.001;
+    newtxt!(zhat, name=name);
+    ks = zeros(eltype(zhat), length(RKT.b), length(IC)); #RK stages (allocate memory)
     #forcing term
-    N = length(zhat)
+    N = length(zhat);
     FD = zeros(length(k)); 
-    FD[[6+1, 7+1, 8+1, 9+1, -6+(N+1), -7+(N+1), -8+(N+1), -9+(N+1)]] .= 0.2
+    FD[[6+1, 7+1, 8+1, 9+1, -6+(N+1), -7+(N+1), -8+(N+1), -9+(N+1)]] .= 0.2;
     #add damping term
     FD[2:end] += -196.61 * (abs.(k[2:end]).^(-8)) - 53.9* (abs.(0.001 * k[2:end]) .^ 16); 
-    FD[1]= -200.0
+    FD[1]= -200.0;
     for t = 1 : M
         zhat = IFRK_step(zhat, h, L, NLfunc, fP, RKT, ks, k, FD)
         if rem(t,every)==1
-            if any(isnan,zhat)  || any(isinf,x)
+            if any(isnan,zhat)  || any(isinf,zhat)
                 error("Blowup!!!")#break
             end
             addtxt!(zhat, name=name)
