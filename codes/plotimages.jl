b8d68298efcfe45af72197956d3697418c611aff
Minah Yang <lucia.yang@colorado.edu>
2019-10-24 14:55:28 -0600

    forcing plot

--- codes/plotimages.jl
+++ codes/plotimages.jl
@@ -1,7 +1,7 @@
 include("readwrite.jl")
 
 using PyPlot, LaTeXStrings
-name = "bigICRK3"
+#=name = "bigICRK3"
 solhat = readCfile(name)
 sol = ifft(solhat, 2)
 
@@ -15,4 +15,12 @@ for i = 1 : 5: T
 	legend()
 	savefig("../plots/"*name*string(i)*".png")
 	close(fig)
-end
+end=#
+
+f200 = readdlm("../txtfiles/f200.txt"); plot(f200, label="0.2");
+f100 = readdlm("../txtfiles/f100.txt"); plot(f100, label="0.1");
+f050 = readdlm("../txtfiles/f050.txt"); plot(f050, label="0.05");
+f025 = readdlm("../txtfiles/f025.txt"); plot(f025, label="0.025");
+f010 = readdlm("../txtfiles/f010.txt"); plot(f010, label="0.010")
+legend()
+title("Varying Forcing")
