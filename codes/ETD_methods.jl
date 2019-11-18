1538a9a6526be1f014613ec7a22c03498b7aed05
Minah Yang <lucia.yang@colorado.edu>
2019-11-18 13:52:16 -0700

    organize ETD/IF's separately

---
+++ codes/ETD_methods.jl
@@ -0,0 +1,40 @@
+include("MMT.jl")
+
+L = [1.0, im*1.0]
+
+#ETDRK2
+A=fill(zeros(2), 1,2)
+A[1,1]=[1,0];A=Array{Any,2}(A)
+c=[1]
+b=[[1,-1],[0,1]];b=Array{Any,1}(b)
+A,b=buildAandb(A,b,c,h,L)
+ETDRK2=ETDRKTableau(A,b,c)
+
+#ETDRK3
+A=fill(zeros(3),2,3);
+A[1,1]=[1/2,0,0];
+A[2,1:2]=[[-1,0,0],[2,0,0]];A=Array{Any,2}(A)
+c=[1/2,1];
+b=[[1.0,-3,4],[0,4,-8],[0,-1,4]];b=Array{Any,1}(b)
+A,b = buildAandb(A,b,c,h,L);
+ETDRK3=ETDRKTableau(A,b,c)
+
+#ETDRK4
+A=fill(zeros(4),3,4);
+A[1,1]=[1/2,0,0,0];
+A[2,2]=[1/2,0,0,0];
+A[3,[1,3]]=[[1/2,0,0,0],[1,0,0,0]];A=Array{Any,2}(A)
+c=[1/2,1/2,1];
+b=[[1,-3,4,0],[0,2,-4,0],[0,2,-4,0],[0,-1,4,0]];b=Array{Any,1}(b);
+A,b=buildAandb(A,b,c,h,L); A[3,1] *= (exp(h/2*Diagonal(L)) -I);
+ETDRK4=ETDRKTableau(A,b,c)
+
+#ETDRK4-B
+A=fill(zeros(4),3,4);
+A[1,1]=[1/2,0,0,0];
+A[2,1:2]=[[1/2,-1,0,0],[0,1,0,0]];
+A[3,[1,3]]=[[1,-2,0,0],[0,2,0,0]];A=Array{Any,2}(A)
+c=[1/2,1/2,1];
+b=[[1,-3,4,0],[0,2,-4,0],[0,2,-4,0],[0,-1,4,0]];b=Array{Any,1}(b);
+A,b=buildAandb(A,b,c,h,L);
+ETDRK4B=ETDRKTableau(A,b,c)
--- codes/simplot.jl
+++ codes/simplot.jl
@@ -14,9 +14,7 @@ kind = vcat(collect(Int(N/2)+2:N), collect(1:Int(N/2)+1));
 kindnz = vcat(collect(Int(N/2)+2:N), collect(2:Int(N/2))); # indexing w/o zero mode
 
 #IC
-x = range(0,stop=2*pi-1/N, length=Int(N));
-IC = randn(ComplexF64, N)*sqrt(N)/1000; IC[1]=0.0; IC = ifft(IC); 
-#IC = cos.(x) + im*sin.(x)
+IC = randn(ComplexF64, N)*sqrt(N)/1000; IC[1]=0.0; IC = ifft(IC);
 
 # Linear operator (depends on k)
 L = -im*abs.(k).^fP.α; #L[Int(N/4+2):Int(3*N/4)].=0
@@ -34,51 +32,36 @@ every = Int(M/1000) # save solution at only 1001 time locations.
 name = "A"
 
 # Set-up ETDRK
-#i.e. ETDRK3
-A=fill(zeros(3),2,3);
-A[1,1]=[1/2,0,0];
-A[2,1:2]=[[-1,0,0],[2,0,0]];A=Array{Any,2}(A)
-c=[1/2,1];
-b=[[1.0,-3,4],[0,4,-8],[0,-1,4]];b=Array{Any,1}(b)
-A,b = buildAandb(A,b,c,h,L);
-ETDRK3=ETDRKTableau(A,b,c)
+include("ETD_methods.jl")
 RKT = ETDRK3
-ETDRK!(M, every, IC, h, L, NLfunc, fP, RKT, k, name=name)
-
+#ETDRK!(M, every, IC, h, L, NLfunc, fP, RKT, k, name=name)
 
 # Set-up IFRK
-A = hcat([0; .5; 0; 0], [0; 0; .5; 0], [0; 0; 0; 1.0]); #3by4
-b = [1/6; 1/3; 1/3; 1/6];
-c = [0; 1/2; 1/2; 1];
-RK4 = eRKTableau(A, b, c);
-A = hcat([0; .5; -1],[0; 0; 2]);
-b = [1/6; 2/3; 1/6];
-c = [0; 1/2; 1];
-RK3 = eRKTableau(A, b, c);
+#include("IF_methods.jl")
 #RKT = RK4;
-# run IKRK
 #IFRK!(M, every, IC, h, L, NLfunc, fP, RKT, k, name=name) 
 
-solhat = readCfile(name)#[11:end,:]
-#sol = ifft(solhat, 2)
-E = k .* transpose(sum(abs.(solhat).^2, dims = 1)/size(solhat)[1])/N^2;
-
+function plotEnergy!(k, N, T, name::String)
+	solhat = readCfile(name)#[11:end,:]
+	#sol = ifft(solhat, 2)
+	E = k .* transpose(sum(abs.(solhat).^2, dims = 1)/size(solhat)[1])/N^2;
+	fig, ax = subplots()
+	#semilogy(k[kindnz], E[kindnz], label="computed")
+	#semilogy(k[2:Int(end/2)], 1e-24 *(k[2:Int(end/2)]).^(-1/3), label=L"Ck^{-1/3}")
+	#semilogy(k[2:Int(end/2)], 1e-24 *(k[2:Int(end/2)]).^(-1/2), label=L"Ck^{-1/2}")
+	loglog(k[2:Int(end/2)], E[2:Int(end/2)], label=L"k\times"*"computed")
+	axhline(0.1, color=:black, label="0.1")
+	#loglog(k[2:Int(end/2)], .24 *(k[2:Int(end/2)]).^(-2), label=L"Ck^{-2}")
+	#loglog(k[2:Int(end/2)], .24 *(k[2:Int(end/2)]).^(-1), label=L"Ck^{-1}")
+	xlabel("Wave Number")
+	ylabel("n(k)")
+	legend()
+	title(L"t\in["*string(0.011*T)*", "*string(T)*"]")
+	savefig(name*"ES.png")
+	close(fig)
+end
 
 using PyPlot, LaTeXStrings
-fig, ax = subplots()
-#semilogy(k[kindnz], E[kindnz], label="computed")
-#semilogy(k[2:Int(end/2)], 1e-24 *(k[2:Int(end/2)]).^(-1/3), label=L"Ck^{-1/3}")
-#semilogy(k[2:Int(end/2)], 1e-24 *(k[2:Int(end/2)]).^(-1/2), label=L"Ck^{-1/2}")
-loglog(k[2:Int(end/2)], E[2:Int(end/2)], label=L"k\times"*"computed")
-axhline(0.1, color=:black, label="0.1")
-#loglog(k[2:Int(end/2)], .24 *(k[2:Int(end/2)]).^(-2), label=L"Ck^{-2}")
-#loglog(k[2:Int(end/2)], .24 *(k[2:Int(end/2)]).^(-1), label=L"Ck^{-1}")
-xlabel("Wave Number")
-ylabel("n(k)")
-legend()
-title(L"t\in["*string(0.011*T)*", "*string(T)*"]")
-savefig(name*"ES.png")
-close(fig)
-
+#plotEnergy!(k, N, T, name)
 
 
