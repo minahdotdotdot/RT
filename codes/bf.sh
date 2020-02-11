a3ab9faceba3ad8f4d33580615fded984b7ebb88
Minah Yang <lucia.yang@colorado.edu>
2020-02-11 10:29:35 -0700

    add rational instructions

--- codes/bf.sh
+++ codes/bf.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for h in 0.06 0.05 0.025 0.01 0.005 0.0025 0.001 0.0005 0.00025 0.0001
+for h in 0.06 #0.05 0.025 0.01 0.005 0.0025 0.001 0.0005 0.00025 0.0001
 do
 	sed -i '3 a h='$h ./setupMMT.jl
 	sed -i '5 d' ./setupMMT.jl
-	sed -i '1 a #SBATCH --job-name I3'$h ./batchfile.sh
-	sed -i '1 d' ./batchfile.sh
-	sbatch batchfile.sh
+	sed -i '1 a #SBATCH --job-name I3R'$h ./batchfile.sh
+	sed -i '3 d' ./batchfile.sh
+	sed -i '15 a matlab -nodesktop -nodisplay -r "addpath(fullfile(cd,'chebfun'));genRationalApprox('IFRK3Rh='$h'd6');exit"' ./batchfile.sh
+	sed -i '17 d' ./batchfile.sh
+	#sbatch batchfile.sh
 done
