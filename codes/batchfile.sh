8e6ba9cbc5c5b51f69c55eeb394b409dda2eb00f
Minah Yang <lucia.yang@colorado.edu>
2020-02-11 11:19:57 -0700

    this one works

--- codes/batchfile.sh
+++ codes/batchfile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name I3R06d6
-#SBATCH --qos=blanca-igg
+#SBATCH --job-name I3R0.025
+#SBATCH --qos=blanca-appm-student
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
-#SBATCH --time 168:00:00  
+#SBATCH --time 24:00:00  
 #SBATCH --mail-type=all
 #SBATCH --mail-user=luya7574@colorado.edu
 
@@ -12,6 +12,4 @@ julia ./setupMMT.jl
 
 module purge
 module load matlab
-matlab -nodesktop -nodisplay -r "addpath(fullfile(cd,'chebfun'));genRationalApprox('IFRK3Rh=0.06d6');exit"
-
-julia ./simplot.jl
+matlab -nodesktop -nodisplay -r "addpath(fullfile(cd,chebfun));genRationalApprox(IFRK3Rh=0.025d6)exit"
--- codes/bf.sh
+++ codes/bf.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-for h in 0.06 #0.05 0.025 0.01 0.005 0.0025 0.001 0.0005 0.00025 0.0001
+#for h in 0.06 #0.05 0.025 0.01 0.005 0.0025 0.001 0.0005 0.00025 0.0001
+for h in 0.05 0.025 0.01 0.005 0.0025 #0.001 0.0005 0.00025 0.0001
+
 do
 	sed -i '3 a h='$h ./setupMMT.jl
 	sed -i '5 d' ./setupMMT.jl
 	sed -i '1 a #SBATCH --job-name I3R'$h ./batchfile.sh
 	sed -i '3 d' ./batchfile.sh
-	sed -i '15 a matlab -nodesktop -nodisplay -r "addpath(fullfile(cd,'chebfun'));genRationalApprox('IFRK3Rh='$h'd6');exit"' ./batchfile.sh
-	sed -i '17 d' ./batchfile.sh
-	#sbatch batchfile.sh
+	cm='"addpath(fullfile(cd,'chebfun'));genRationalApprox('IFRK3Rh=''
+        cm2=''d6')exit"'
+	sed -i '14 a matlab -nodesktop -nodisplay -r '${cm}$h${cm2} ./batchfile.sh 
+	#sed -i '14 a matlab -nodesktop -nodisplay -r "addpath(fullfile(cd,'chebfun'));genRationalApprox('IFRK3Rh='${h}'d6');exit"' ./batchfile.sh
+	sed -i '16 d' ./batchfile.sh
+	sbatch batchfile.sh
+	sleep 2m
 done
