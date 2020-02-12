ded96f2e2d494ce49467a6bea82e537cd99e91d8
Minah Yang <lucia.yang@colorado.edu>
2020-02-12 15:08:56 -0700

    m

--- codes/genRationalApprox.m
+++ codes/genRationalApprox.m
@@ -7,10 +7,11 @@ function genRationalApprox(name)
     s = size(file.x);
     for i = 1 : s(1)
         for j = 1: s(1)-1
-            Z = exp(h*file.x(i,j)*file.L);
+            L = h*file.x(i,j)*file.L;
+            Z = exp(L);
             [r, pol, res, zer, zj, fj, wj] = aaa(Z,h*file.x(i,j)*file.L,'degree', deg);
             crat(i,j) = {r(h*file.x(i,j)*file.L)-r([0])+1};
         end
     end
-    save(join(['../data/',name,'R.mat'],''), 'crat');
+    save(join(['../data/',name,'R2.mat'],''), 'crat');
 end
