5eab2fbedab40f8c04b8391460f9a75db362f60a
Minah Yang <lucia.yang@colorado.edu>
2020-02-13 17:30:59 -0700

    allow convergent and not convergent

--- codes/genRationalApprox.m
+++ codes/genRationalApprox.m
@@ -4,14 +4,25 @@ function genRationalApprox(name)
     h=nf.h; deg=nf.deg;
     file = load(join(['../data/',name,'.mat'],''));
     crat = file.crat;
+    crat2 = file.crat
     s = size(file.x);
+    uniquex = unique(file.x);
+    c = containers.Map('KeyType', 'double', 'ValueType', 'any')
+    c2 = containers.Map('KeyType', 'double', 'ValueType', 'any')
+    for i = 1 : length(uniquex)
+        L = h*uniquex(i)*file.L;
+        Z = exp(L);
+        [r, pol, res, zer, zj, fj, wj] = aaa(Z,L,'degree', deg);
+        c(uniquex(i)) = r(L) - r(0) +1;
+        c2(uniquex(i)) = r(L);
+    end
+
     for i = 1 : s(1)
         for j = 1: s(1)-1
-            L = h*file.x(i,j)*file.L;
-            Z = exp(L);
-            [r, pol, res, zer, zj, fj, wj] = aaa(Z,h*file.x(i,j)*file.L,'degree', deg);
-            crat(i,j) = {r(h*file.x(i,j)*file.L)-r([0])+1};
+            crat(i,j) = {c(file.x(i,j))};
+            crat2(i,j) = {c2(file.x(i,j))}
         end
     end
-    save(join(['../data/',name,'R2.mat'],''), 'crat');
+    save(join(['../data/',name,'R.mat'],''), 'crat');
+    save(join(['../data/',name,'R2.mat'],''), 'crat2');
 end
