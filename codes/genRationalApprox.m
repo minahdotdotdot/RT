2180f5201db2aaf14a55c6f1f3f9295689c754cd
Minah Yang <lucia.yang@colorado.edu>
2020-02-14 17:29:43 -0700

    rational domain is all L

--- codes/genRationalApprox.m
+++ codes/genRationalApprox.m
@@ -7,6 +7,27 @@ function genRationalApprox(name)
     crat2 = file.crat;
     s = size(file.x);
     uniquex = unique(file.x);
+    L = [];
+    for i = 1 : length(uniquex)
+        L = [L h*uniquex(i)*file.L]
+    end
+    Z = exp(L)
+    [r, pol, res, zer, zj, fj, wj] = aaa(Z,L,'degree', deg);
+
+    for i = 1 : length(uniquex)
+        L = h*uniquex(i)*file.L;
+        c(uniquex(i)) = r(L) - r(0) +1;
+        c2(uniquex(i)) = r(L);
+    end
+
+    for i = 1 : s(1)
+        for j = 1: s(1)-1
+            crat(i,j) = {c(file.x(i,j))};
+            crat2(i,j) = {c2(file.x(i,j))};
+        end
+    end
+
+    %{
     c = containers.Map('KeyType', 'double', 'ValueType', 'any');
     c2 = containers.Map('KeyType', 'double', 'ValueType', 'any');
     for i = 1 : length(uniquex)
@@ -25,4 +46,5 @@ function genRationalApprox(name)
     end
     save(join(['../data/',name,'R.mat'],''), 'crat');
     save(join(['../data/',name,'R2.mat'],''), 'crat2');
+%}
 end
