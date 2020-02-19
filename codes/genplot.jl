d211432440d83fde228f6b465c328187a1bad22c
Minah Yang <lucia.yang@colorado.edu>
2020-02-19 11:21:29 -0700

    plot functions

--- codes/genplot.jl
+++ codes/genplot.jl
@@ -11,7 +11,9 @@ mdict = Dict{String, String}();
 cdict = Dict{String, Symbol}();
 mds = ["IFRK3" "ETDRK3" "ARK3" "ARK4"]
 cs = [:red; :orange; :green; :blue]
-hs = [0.06 0.05 0.025 0.01 0.005 0.0025 0.001 0.0005 0.00025 0.0001 0.000075 0.00005]
+hs = [0.06; 0.05; 0.025; 0.01;
+0.005; 0.0025; 0.001; 0.0005;
+0.00025; 0.0001; 0.000075; 0.00005]
 for (i,m) in enumerate(mds)
     for h in hs
         push!(hdict, m*"-"*string(Int(h*1000000),pad=6) => h)
@@ -22,7 +24,6 @@ end
 
 ddict=Dict{String, Int64}();
 cs2 = [:purple, :pink, :grey]
-hs2 = [0.06 0.05 0.025 0.01 0.005 0.0025 0.001 0.0005 0.00025 0.0001 0.000075 0.00005]
 ds = [4 6 8]
 for h in hs
     for (i,d) in enumerate(ds)
@@ -45,11 +46,14 @@ ARK4list = ["ARK4-060000", "ARK4-050000", "ARK4-025000", "ARK4-010000",
 "ARK4-005000", "ARK4-002500", "ARK4-001000", "ARK4-000500", 
 "ARK4-000250", "ARK4-000100"];
 IFr4list = ["IFRK3R-060000-d4", "IFRK3R-050000-d4", "IFRK3R-025000-d4", "IFRK3R-010000-d4",
-"IFRK3R-005000-d4", "IFRK3R-002500-d4", "IFRK3R-001000-d4", "IFRK3R-000500-d4"];
+"IFRK3R-005000-d4", "IFRK3R-002500-d4", "IFRK3R-001000-d4", "IFRK3R-000500-d4",
+"IFRK3R-000250-d4"];
 IFr6list = ["IFRK3R-060000-d6", "IFRK3R-050000-d6", "IFRK3R-025000-d6", "IFRK3R-010000-d6",
-"IFRK3R-005000-d6", "IFRK3R-002500-d6", "IFRK3R-001000-d6", "IFRK3R-000500-d6"];
+"IFRK3R-005000-d6", "IFRK3R-002500-d6", "IFRK3R-001000-d6", "IFRK3R-000500-d6",
+"IFRK3R-000250-d6"];
 IFr8list = ["IFRK3R-060000-d8", "IFRK3R-050000-d8", "IFRK3R-025000-d8", "IFRK3R-010000-d8",
-"IFRK3R-005000-d8", "IFRK3R-002500-d8", "IFRK3R-001000-d8", "IFRK3R-000500-d8"];
+"IFRK3R-005000-d8", "IFRK3R-002500-d8", "IFRK3R-001000-d8", "IFRK3R-000500-d8",
+"IFRK3R-000250-d8"];
 
 listn = vcat(ETDlist, ARK3list, ARK4list, IFr8list, IFr6list, IFlist[1:end-1], IFr4list);
 
@@ -110,14 +114,50 @@ IFr4err = saveerror(k, IFr4list, "IFRK3-000100",2, true, errtype="Matrix", hdict
 IFr6err = saveerror(k, IFr6list, "IFRK3-000100",2, true, errtype="Matrix", hdict=hdict);
 IFr8err = saveerror(k, IFr8list, "IFRK3-000100",2, true, errtype="Matrix", hdict=hdict);
 
+function plotErrRationals!(k, hs::Vector{T}; nt::Float64=2.0) where T<:AbstractFloat
+    fig, ax = subplots()
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    k2 = k[2:Int(end/2)]
+    truth = log.(readdlm("../txtfiles/IFRK3-000100.txt")' ./ k2)
+    for h in hs
+        #truth = log.(readdlm("../txtfiles/IFRK3-"*string(Int(h*1000000),pad=6)*".txt")' ./ k2)
+        for i = 3 : -1 : 1
+            E = log.(readdlm("../txtfiles/IFRK3R-"*string(Int(h*1000000),pad=6)*"-d"*string(2*i+2)*".txt")' ./ k2)
+            err = norm(truth-E,nt)/norm(truth,nt)
+            scatter(h, err, c=cs2[i], s=40*i-20)
+        end
+    end
+    for i = 1 : 3
+        scatter([], [], c=cs2[i], s=40*i-20, label="deg="*string(2*i+2))
+    end
+    xlabel("h: time-step size")
+    ylabel("Relative error ("*string(nt)*"-norm)")
+    title("Error in log(ES) of rational approx against IFRK3, h=0.0001")
+    legend()
+    xlim(0.5*minimum(hs), 1.5*maximum(hs))
+    ylim(2e-3, 1e-2)
+    ax.set_axisbelow(true)
+    ax.grid(true)
+    # Turn on the minor TICKS, which are required for the minor GRID
+    ax.minorticks_on()
+
+    # Customize the major grid
+    ax.grid(which="major", axis="both", linestyle="-", linewidth=0.5, color=:red)
+    # Customize the minor grid
+    ax.grid(which="minor", axis="both", linestyle=":", linewidth=0.5, 
+        color=:black, alpha=0.7)
+
+end
+
 function plotErrvH!(k, name::Vector{String}, hdict, mdict;#, m::Int, n::Int
-    tru::String="IFRK3-000100", regfit::Bool=false, minV=0)  
+    tru::String="IFRK3-000100", regfit::Bool=false, minV=0, nt::Float64=2.0)  
     k2 = k[2:Int(end/2)]
     truth=log.(readdlm("../txtfiles/"*tru*".txt")' ./ k2)
     #fig, ax = subplots()#(m,n); #ymin=-1; ymax=-1
     for (i,na) in enumerate(name)
         E = log.(readdlm("../txtfiles/"*na*".txt")' ./ k2)
-        err = norm(truth-E,2)/norm(truth,2)
+        err = norm(truth-E,nt)/norm(truth,nt)
         if mdict[na] == "IFRK3R"
             scatter(hdict[na], err, c=cdict[na], s=(ddict[na]-4)*20+20)
         else
@@ -136,40 +176,41 @@ function plotErrvH!(k, name::Vector{String}, hdict, mdict;#, m::Int, n::Int
         end=#
     end 
     if regfit == true
-        err = saveerror(k, name, tru,2, true, errtype="Matrix", hdict=hdict);
+        err = saveerror(k, name, tru,nt, true, errtype="Matrix", hdict=hdict);
         data = DataFrame(logh=err[:,1], logerr=err[:,2]);
         ols=lm(@formula(logh ~ logerr), data);
         errrange = range(minV, stop=maximum(err), length=1001)
         fitline = exp.(predict(ols, DataFrame(logerr=errrange)))
         #axvline(fitline[1], c=cdict[name[1]])
-        plot(fitline, exp.(errrange), c=cdict[name[1]], linewidth=0.75)#, linestyle=":"    
-        scatter(fitline[1], exp.(errrange)[1], c=cdict[name[1]], marker="x")  
+        plot(fitline, exp.(errrange), c=cdict[name[1]], linewidth=0.75) 
+        scatter(fitline[1], exp.(errrange)[1], c=cdict[name[1]], marker="x") 
     end
 end
 
-function plotErrvH2!()
+function plotErrvH2!(nt::Float64=2.)
     fig,ax = subplots()
-    err = saveerror(k, IFlist[1:end-1], "IFRK3-000100", 2, true, errtype="Matrix", hdict=hdict);
+    err = saveerror(k, IFlist[1:end-1], "IFRK3-000100", Inf, true, errtype="Matrix", hdict=hdict);
     minV=minimum(err[:,2]);
     #print(minV)
     ax.set_xscale("log")
     ax.set_yscale("log")
-    axhline(y=exp(minV),c=:red)
-    plotErrvH!(k,ETDlist, hdict, mdict, regfit=true, minV=minV)
-    plotErrvH!(k,ARK3list, hdict, mdict, regfit=true, minV=minV)
-    plotErrvH!(k,ARK4list, hdict, mdict, regfit=true, minV=minV)
-    plotErrvH!(k,IFr8list, hdict, mdict, regfit=false)
-    plotErrvH!(k,IFr6list, hdict, mdict, regfit=false)
-    plotErrvH!(k,IFlist[1:end-1], hdict, mdict, regfit=false)
-    plotErrvH!(k,IFr4list, hdict, mdict, regfit=false)
+    axhline(y=exp(minV),c=:red, label="Minimum Error")
+    plotErrvH!(k,ETDlist, hdict, mdict, regfit=true, minV=minV, nt=Inf)
+    plotErrvH!(k,ARK3list, hdict, mdict, regfit=true, minV=minV, nt=Inf)
+    plotErrvH!(k,ARK4list, hdict, mdict, regfit=true, minV=minV, nt=Inf)
+    #plotErrvH!(k,IFr8list, hdict, mdict, regfit=false, nt=Inf)
+    #plotErrvH!(k,IFr6list, hdict, mdict, regfit=false, nt=Inf)
+    plotErrvH!(k,IFlist[1:end-1], hdict, mdict, regfit=false, nt=Inf)
+    #plotErrvH!(k,IFr4list, hdict, mdict, regfit=false, nt=Inf)
     for (i,m) in enumerate(mds)
         scatter([], [], c=cs[i], label=m)
     end
-    for (i,d) in enumerate(ds)
+    #=for (i,d) in enumerate(ds)
         scatter([], [], c=cs2[i], label="deg="*string(d), s=(d-4)*20+20)
-    end
+    end=#
+    plot([], [], c=:black, marker="x", label="fitted")
     xlabel("h: time-step size")
-    ylabel("Relative error (2-norm)")
+    ylabel("Relative error ("*string(nt)*"-norm)")
     title("Error in log(Energy Spectrum)")
     legend()
     ax.set_axisbelow(true)
