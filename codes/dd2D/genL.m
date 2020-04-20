d03b3778f3984be04dedc6678abbba55d2664deb
Minah Yang <lucia.yang@colorado.edu>
2020-04-20 13:31:29 -0600

    make N/2 = 0

--- codes/dd2D/genL.m
+++ codes/dd2D/genL.m
@@ -6,16 +6,16 @@ function bigL = genL(ks, ms, Rrho, Sc, tau, Nx, Nz)
     for jj = 1 : Nz
         for ii = 1 : Nx
             kk = (jj-1)*Nx+ii;
-            bigL((kk-1)*3+1:3*kk, (kk-1)*3+1:3*kk)=gen3by3L(ks(ii), ms(jj), Rrho, Sc, tau);
+            bigL((kk-1)*3+1:3*kk, (kk-1)*3+1:3*kk)=gen3by3L(ks(ii), ms(jj), Rrho, Sc, tau, Nx);
         end
     end
 end
 
 
 % This is a 3-by-3 block of L_{k,m}.
-function lilL = gen3by3L(k, m, Rrho, Sc, tau)
+function lilL = gen3by3L(k, m, Rrho, Sc, tau, Nx)
     km = k^2+m^2;
-    if km == 0
+    if km == 0 || k == Nx/2
         lilL = zeros(3,3);
     else
         lilL = [[-Sc*km; -1i*k; -1i*k]...
