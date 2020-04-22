028fc92a67a1f9a464850ace86dfcbef48e367ee
Minah Yang <lucia.yang@colorado.edu>
2020-04-22 17:59:24 -0600

    works serially

--- codes/dd2D/dd2D_IF.m
+++ codes/dd2D/dd2D_IF.m
@@ -2,11 +2,11 @@ run dd2D
 %Time-stepping 
 T = 10;
 h = 5e-2;
-M = T/h;
+M = 500;%T/h;
 x = xIC;
 every = 100;
 
-parpool
+%parpool
 %linear operator
 %L = genL(ks, ms, Rrho, Sc, tau, Nx, Nz);
 xf = IFRK(xIC, M, h, Nx, Nz, ks, ms, km, every, "RK3", Rrho, Sc, tau);
