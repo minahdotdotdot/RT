20e9c47e09397ec18a2e94209ea07eed43c855f2
Minah Yang <lucia.yang@colorado.edu>
2020-05-04 22:36:36 -0600

    add parallel to genL

--- codes/dd2D/genL.m
+++ codes/dd2D/genL.m
@@ -1,13 +1,43 @@
 %% Linear tendency
 % This creates a giant sparse operator L.
 
-function bigL = genL(pp, dp)%ks, ms, Rrho, Sc, tau, Nx, Nz)
+function bigL = genL(pp, dp, par)
     bigL=sparse(3*dp.Nx*dp.Nz, 3*dp.Nx*dp.Nz);
-    for jj = 1 : dp.Nz
-        for ii = 1 : dp.Nx
-            kk = (jj-1)*dp.Nx+ii;
-            bigL((kk-1)*3+1:3*kk, (kk-1)*3+1:3*kk)=gen3by3L(dp.ks(ii), dp.ms(jj), pp, dp);
-            %(ks(ii), ms(jj), Rrho, Sc, tau, Nx);
+    if par == 0
+        for jj = 1 : dp.Nz
+            for ii = 1 : dp.Nx
+                kk = (jj-1)*dp.Nx+ii;
+                bigL((kk-1)*3+1:3*kk, (kk-1)*3+1:3*kk)=gen3by3L(dp.ks(ii), dp.ms(jj), pp, dp);
+                %(ks(ii), ms(jj), Rrho, Sc, tau, Nx);
+            end
+        end
+    else
+        delete(gcp('nocreate'))
+        parpool(workers)
+        workers =par(1); m=3*dp.Nx*dp*Nz;
+        if mod(m, workers) == 0
+            p = m / workers;
+            blocks = cell(workers,1);
+            parfor ii = 1 : workers
+                for jj = 1 : p
+                    kk = (jj-1)*workers + ii;
+                    if jj == 1
+                        blocks{ii} = cell(p,1);
+                    end
+                    yy = floor(kk/Nx); xx = kk-(yy-1)dp.Nx;
+                    blocks{ii}{jj}=gen3by3L(dp.ks(xx), dp.ms(yy), pp, dp);
+                end
+            end
+            expL = sparse(m,n);
+            for kk = 1 : workers*p
+                jj= ceil(kk/workers); ii = kk - (jj-1)*workers;
+                bigL((kk-1)*3+1:3*kk, (kk-1)*3+1:3*kk) = blocks{ii}{jj};
+            end
+            if issparse(bigL) == false
+                expL = sparse(bigL);
+            end
+        else
+            error('check m/workers')
         end
     end
 end
