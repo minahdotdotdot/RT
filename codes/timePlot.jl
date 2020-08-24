a43156c9d21e27554091e4c9f2785a3a7391a683
Minah Yang <lucia.yang@colorado.edu>
2020-08-24 16:58:19 -0600

    example plot code

--- codes/timePlot.jl
+++ codes/timePlot.jl
@@ -1,3 +1,8 @@
 using DelimitedFiles
-na="IFRK3-000500"
-E=readdlm("../txtfiles/"*na*".txt")
+include("readwrite.jl")
+na="IFRK3-000100"
+sol = readCfile(na);
+N = 2^13;
+totenergy = sum(sol .* conj.(sol), dims=2)/N^2;
+semilogy(0:length(totenergy),totenergy)
+
