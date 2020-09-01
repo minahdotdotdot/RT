f4b13ee5c7051a158fc3b49916a2f8794806f000
MinahThinkpad <lucia.yang@colorado.edu>
2020-08-31 18:57:45 -0600

    eps fix

--- codes/eps.jl
+++ codes/eps.jl
@@ -1,12 +1,10 @@
 include("readwrite.jl")
-α=1/2;
-β=0;
 zhat = readCfile("IFRK3-000100");
 include("setupMMT.jl")
 E = zeros(size(zhat)[1])
 HNL = zeros(size(zhat)[1])
 for i = 1 : size(zhat)[1]
-	E[i] = norm(ifft((abs.(k).^(α/2)) .*zhat[i,:],2))^2;
-	HNL[i] = norm(ifft((abs.(k).^(β/4)) .*zhat[i,:],2))^4;
+	E[i] = sum(abs.(ifft((abs.(k).^(fp.α/2)) .*zhat[i,:])).^2);
+	HNL[i] = .5*fp.λ*sum(abs.(ifft((abs.(k).^(fp.β/4)) .*zhat[i,:])).^4);
 end
 ϵ = E ./ HNL;
