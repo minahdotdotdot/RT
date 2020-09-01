0c1aa6de2f0a4bd2ac69d62871518bbf99f5d3d9
MinahThinkpad <lucia.yang@colorado.edu>
2020-08-31 18:58:44 -0600

    fp to fP

--- codes/eps.jl
+++ codes/eps.jl
@@ -4,7 +4,7 @@ include("setupMMT.jl")
 E = zeros(size(zhat)[1])
 HNL = zeros(size(zhat)[1])
 for i = 1 : size(zhat)[1]
-	E[i] = sum(abs.(ifft((abs.(k).^(fp.α/2)) .*zhat[i,:])).^2);
-	HNL[i] = .5*fp.λ*sum(abs.(ifft((abs.(k).^(fp.β/4)) .*zhat[i,:])).^4);
+	E[i] = sum(abs.(ifft((abs.(k).^(fP.α/2)) .*zhat[i,:])).^2);
+	HNL[i] = .5*fP.λ*sum(abs.(ifft((abs.(k).^(fP.β/4)) .*zhat[i,:])).^4);
 end
 ϵ = E ./ HNL;
