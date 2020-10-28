d7a5ed66a384743b1d79bc5f4a99dad1970266ec
MinahThinkpad <lucia.yang@colorado.edu>
2020-10-28 11:34:05 -0600

    move unnecessary files to old

---
+++ codes/dd2D/old/dd2D_IFsetup.m
@@ -0,0 +1,44 @@
+run dd2D
+%Time-stepping 
+%h = 2e-2; 
+dname=join(['D32N',sprintf('%02d',log2(dp.Nx)),'P1'],'');%dname='D32N07P1'
+bname = join(['I3',sprintf('%03d',h*1e3)],'');
+%% Timings for reference
+%parpool %parpool only called in fillc
+%linear operator
+%workers = 8; %3*Nx*Nz = 3(2^{2p+log_2(aspectratio)}) 
+%i.e. aspect ratio = 2, p = 7 --> 3NxNz = 3(2^{15})
+%bs = 3*2^5; % block size: log_2(15 - 3 = 12)
+%N=2^7, aspectratio = 2 -> bs = 3*2^7 || 32  sec
+%                       -> bs = 3*2^6 || 28  sec
+%                       -> bs = 3*2^5 || 5.3 sec
+%                       -> bs = 3*2^4 || 6.2 sec
+
+workers = 4;
+%i.e. aspect ratio = 1, p = 10 --> 3NxNz = 3(2^{20})
+bs = 3*2^7; % Each worker does: TOTAL: 3*2^{20 - 4 = 16} since workers=16=2^4
+%                             AT ITER: bs=3*2^7, 2^{16-7} ITERs
+%p=log_2(N) :  3NxNz   -> genL  || expL
+%  7        : 3*2^{14} ->   25s ||   1.4s
+%  8        : 3*2^{16} ->   21s ||   3.5s
+%  9        : 3*2^{18} ->   22s ||  20. s
+% 10        : 3*2^{20} -> 1637s ||    . s
+
+%par = 0;tic
+%L=genL(pp,dp,par);toc
+%% Load Linear operator
+data = load(join(['../../data/','D32N',sprintf('%02d',log2(dp.Nx)),'P1L.mat'],''));
+L = data.L; clear data;
+par = workers;
+%{
+tic
+delete(gcp('nocreate'))
+parpool(workers)
+L2 = genL(pp, dp, par);toc;
+%}
+pname = join(['../../data/',bname,dname,'params.mat'],'')
+save(pname,'L');
+%% Set up RK scheme and exp(chL)'s
+RK = genIFRKexpL("RK3", h, L, workers, bs, pp, dp);
+run setupNL
+save(pname,'RK', 'dpNL','-append');
---
+++ codes/dd2D/old/dd2D_ex.m
@@ -0,0 +1,31 @@
+run dd2D
+%% Set up Linear Operator
+L = genL(pp,dp);
+eigs = eigL(pp,dp);
+%% Set up Nonlinear stuff.
+run setupNL
+
+
+
+T = 1.06;
+h = 0.9/max(max(abs(eigs)))
+M = T/h;
+a=load('RK4.mat');
+x = a.xf;
+every = 50;
+[xf,ES,FS] = RK4(x, M, h, every, L, dpNL);
+save('RK4-continued.mat', 'xf');
+
+%{
+A_RK4 = [[0; .5; 0; 0] [0; 0; .5; 0] [0; 0; 0; 1.0]];
+b_RK4 = [1/6; 1/3; 1/3; 1/6];
+c_RK4 = [0; 1/2; 1/2; 1]; 
+for ii = 1 : M
+	if sum(isnan(x)) == [0 0 0]
+		%x = exRK(x, A_RK4, b_RK4, c_RK4, h, L, Nx, Nz, dx, km);
+		x = RK4(x, h, L, Nx, Nz, dx, km);
+	else
+		break
+	end
+end
+%}
