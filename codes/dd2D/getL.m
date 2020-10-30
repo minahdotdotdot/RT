bd46136a2edab7534767b62ff36ad2f143bb5596
Minah Yang <lucia.yang@colorado.edu>
2020-10-30 10:37:55 -0600

    change arg for genL`

--- codes/dd2D/getL.m
+++ codes/dd2D/getL.m
@@ -6,7 +6,7 @@ function L = getL(N,D,Pr,dp,pp,dname,p)
 		L=data.L;
 	else
 		disp('L file does not exist, generating it now.');
-		L = genL(pp,dp,p(3));
+		L = genL(pp,dp,p(2));
 		pname = '../../data/'+p(1)+p(2)+'params.mat';
 		save(pname,'L');
 	end
